bb00ddfb26d1a758fa672ce1a6dd1582f885dc05
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-23 20:53:20 +0500

    add express-asyncHandler package to handle async operations in routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,24 +1,35 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import Product from '../models/Product.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+//* @desc Fetch all products
+//* @route GET /api/products
+//* @access Public
 
-router.get('/:id', async (req, res) => {
-  try {
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+//* @desc Fetch a single product
+//* @route GET /api/products/:id
+//* @access Public
+
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  })
+);
 
 export default router;
