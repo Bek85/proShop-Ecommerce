49c0e5edd97a127530498454a524fb7efb31a3d8
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-23 19:25:48 +0500

    create product route

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoute from './routes/product.js';
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,14 +10,7 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoute);
 
 app.listen(
   PORT,
---
+++ backend/routes/product.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import Product from '../models/Product.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+export default router;
