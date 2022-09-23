f9d6bc4849efd4fb107f0c9aad82152d28bfecc6
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-23 19:10:30 +0500

    create data seeder script to populate mongodb database with sample data

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: 'proShop',
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
