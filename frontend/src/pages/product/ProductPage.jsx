ef92eebf43782c79509231c3cf2d7201275a119c
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-23 14:53:34 +0500

    create ProductPage and populate it with static data from products.js

--- frontend/src/pages/product/ProductPage.jsx
+++ frontend/src/pages/product/ProductPage.jsx
@@ -1,3 +1,76 @@
-export default function Product() {
-  return <div>Product</div>;
+import {
+  Button,
+  Card,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../../components/Rating';
+import products from '../../../products';
+
+export default function ProductPage() {
+  let { id } = useParams();
+  const product = products.find((product) => product._id === id);
+
+  return (
+    <>
+      <Link to="/" className="btn btn-dark my-3">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+          </ListGroup>
+          <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
