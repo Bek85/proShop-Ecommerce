24d3804bbc85dd7a5ed3e4ef5edd53d405d0751c
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-23 16:32:01 +0500

    use ES module imports in backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
