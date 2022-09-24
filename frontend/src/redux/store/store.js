3561ec2d0237f18d2b5298f74d561af2e19fb058
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-24 12:34:19 +0500

    create reducer and actions for product list

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { productReducer } from '../reducers/productReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productReducer,
+});
 const initialState = {};
 
 const middleware = [thunk];
