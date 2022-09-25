7cc474cdd57bf3e4c045fb240d8a099dba096d21
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-25 18:22:14 +0500

    create custom authentication middleware to protect routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import generateToken from '../utils/generateToken.js';
 
 //* @desc Auth user & get token
@@ -24,4 +24,24 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//* @desc Get user profile
+//* @route GET /api/users/profile
+//* @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile };
