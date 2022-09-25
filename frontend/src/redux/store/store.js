c92df18362e15a67e9fabcb2575302bcb8043fdf
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-25 11:42:29 +0500

    create cart reducer and actions

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -5,12 +5,21 @@ import {
   productListReducer,
   productDetailsReducer,
 } from 'pro-shop/redux/reducers/productReducers';
+import { cartReducer } from 'pro-shop/redux/reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
-const initialState = {};
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
