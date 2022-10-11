5f8f31c252ef624b875d9d5037ca7e43bf9eff6a
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-10-11 17:45:55 +0500

    create an endpoint to get an order by id

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderbyId } from '../controllers/orderController.js';
 
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderbyId);
 
 export default router;
