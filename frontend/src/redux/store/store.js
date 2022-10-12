c81bd5e9b3c47b68c4ffdc9bdb1b1edd2c931162
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-10-12 14:10:19 +0500

    create orderDetails reducer and actions

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -12,7 +12,10 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from 'pro-shop/redux/reducers/userReducers';
-import { orderCreateReducer } from 'pro-shop/redux/reducers/orderReducers';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from 'pro-shop/redux/reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
