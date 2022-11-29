d276db8719f7f6a9c2e234def8d084c1357ac0f5
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-11-29 21:42:26 +0500

    fixes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import User from '../models/User.js';
+import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
 
 //* @desc Register new user
