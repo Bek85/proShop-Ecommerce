cc72f466d6954f3cfabeadbce336abc7d5ddf313
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-11-29 21:35:16 +0500

    fix

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import Product from '../models/product.js';
+import Product from '../models/Product.js';
 
 //* @desc Fetch all products
 //* @route GET /api/products
