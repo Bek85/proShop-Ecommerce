07309e2c2a8e9c8ea8c886e3897026de5ccce2a9
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-11-29 21:33:35 +0500

    a fix

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
-import { addOrderItems, getOrderbyId } from '../controllers/orderController.js';
+import { addOrderItems, getOrderbyId } from '../controllers/OrderController.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderbyId);
