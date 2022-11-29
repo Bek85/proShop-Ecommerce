6ebaafb3e97c08a24e4d0842b84205ab2843528c
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-11-29 21:36:24 +0500

    fixes

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import Order from '../models/order.js';
+import Order from '../models/Order.js';
 
 //* @desc Create new order
 //* @route POST /api/orders
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import User from '../models/user.js';
+import User from '../models/User.js';
 import generateToken from '../utils/generateToken.js';
 
 //* @desc Register new user
