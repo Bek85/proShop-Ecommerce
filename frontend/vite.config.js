b545982ab7bcac11115da379fd6840185962a36e
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-11-30 23:34:21 +0500

    add eslint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 
@@ -15,6 +16,6 @@ export default ({ mode }) => {
         '/api': 'http://localhost:8000/',
       },
     },
-    plugins: [react()],
+    plugins: [react(), eslint()],
   });
 };
