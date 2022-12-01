2ddaa56139dfd5611be4ffa20f629c7b495c26ec
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-01 10:17:19 +0500

    create paid endpoints on the server

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
-import { addOrderItems, getOrderbyId } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderbyId,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderbyId);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
